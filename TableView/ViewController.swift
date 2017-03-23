1ab7317ed57f77c2d21dced41894b998664f4615
PaulFoxtrot <foxtrot@pdnk.org>
2017-03-23 20:55:29 +0700

    16 cocks in the rows

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
     @IBOutlet weak var firstTableView: UITableView!
     
+    var emojisArray = ["🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔","🐔"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,22 +25,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 30
+        return emojisArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "🦊"
+        cell.textLabel?.text = emojisArray[indexPath.row]
         return cell
     }
     
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
